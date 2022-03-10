ea2f1fca95c57d38e6ca66f83786dd6474bcf35e
stone-punch <66021109+stone-punch@users.noreply.github.com>
2022-03-10 16:55:28 +0900

    commit
    commit

---
+++ 02/app.js
@@ -0,0 +1,41 @@
+//이름 찍기
+var name = 'aj';
+
+//name = 111;
+
+console.log(name);
+
+// 불린형
+var isman = 'true';
+var isman1 = true;
+console.log(isman);
+console.log(isman1);
+
+//함수
+function log(str) {
+    console.log(str);
+  }
+
+  log('hello');
+
+//객체
+var image = {}; 
+
+image.width = 100;
+image.height = 200;
+console.log(image);
+
+//연산자
+console.log(1+1);
+console.log(name + 'hello');
+
+//객체
+var me = {
+    age : 25,
+    name: 'aj',
+    isman : true,
+    sayhello: function(){
+        console.log('hello');
+    },
+};
+
