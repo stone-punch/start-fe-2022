21921944c57459fd04a6e0edd4bb64342ac5ce40
stone-punch <66021109+stone-punch@users.noreply.github.com>
2022-05-19 17:45:19 +0900

    12

---
+++ 12/bak/app.js
@@ -0,0 +1,7 @@
+import sum1 from './sum.js';
+import {sum2} from './sum.js';
+
+
+console.log(sum1(1, 2));
+
+console.log(sum2(1, 4));
---
+++ 12/bak/main.js
@@ -0,0 +1,2 @@
+const sum = require('./common.js');
+console.log(sum(1, 2));
